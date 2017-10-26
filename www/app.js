eef85f4ad0e912a3be4000a2e54a00a697a89624
Ata Tangban <atatangban@live.com>
2017-10-26 12:16:42 +0100

    mvc restructure

--- www/app.js
+++ www/app.js
@@ -38,22 +38,22 @@ app.get('/', function(req, res) {
 });
 
 
-app.get('/write', function(req, res) {
-	res.sendFile(__dirname + "/write.html");
-});
+// app.get('/write', function(req, res) {
+// 	res.sendFile(__dirname + "/write.html");
+// });
 
-app.get('/home', function(req, res) {
-	res.sendFile(__dirname + "/index.html");
-});
+// app.get('/home', function(req, res) {
+// 	res.sendFile(__dirname + "/index.html");
+// });
 
-app.get('/post', function(req, res) {
-	res.sendFile(__dirname + "/post.html");
-});
+// app.get('/post', function(req, res) {
+// 	res.sendFile(__dirname + "/post.html");
+// });
 
-app.post('/write', function(req, res) {
-	let title 	= req.body['title'],
-			content = req.body['content'],
-			timestamp = new Date();
+// app.post('/write', function(req, res) {
+// 	let title 	= req.body['title'],
+// 			content = req.body['content'],
+// 			timestamp = new Date();
 
 	blogRealm.write(() => {
 		blogRealm.create('Post', {title: title, content: content, timestamp: timestamp});
---
+++ www/routes/index.js
@@ -0,0 +1,25 @@
+const routes = require('express').Router();
+
+routes.get('/', (req, res) => {
+  res.status(200).json({ message: 'Connected!' });
+});
+
+routes.get('/write', function(req, res) {
+	res.sendFile(__dirname + "/write.html");
+});
+
+routes.get('/home', function(req, res) {
+	res.sendFile(__dirname + "/index.html");
+});
+
+routes.get('/post', function(req, res) {
+	res.sendFile(__dirname + "/post.html");
+});
+
+routes.post('/write', function(req, res) {
+	let title 	= req.body['title'],
+			content = req.body['content'],
+			timestamp = new Date();
+});
+
+module.exports = routes;
